5ac52906844b65275ff97d9632b522fe6ef0ad63
Malith Dilshan <IT21166556@my.sliit.lk>
2024-12-27 21:33:03 +0530

    added a comment for demonstration. 👍

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ app.use(bodyParser.json());
 
 app.use((req, res, next) => {
   let token = req.header("Authorization");
-
- 
+  //created the auth 
 
   if (token != null) {
     token = token.replace("Bearer ", ""); 
